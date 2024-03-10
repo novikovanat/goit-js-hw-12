b4cd582eab0c30b92802b591bbd1fc60559bacee
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-03-10 20:58:27 +0100

    paging added

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,31 +3,17 @@ import axios from "axios";
 
 //  function for HTTP requests.
 
-export default function fetchPhotos(search){
-// const searchParams = new URLSearchParams({
-//     key :"42747257-6cb1908b03b224c2fc7af7612",
-//     q: `"${search}"`,
-//     image_type: "photo",
-//     orientation:"horizontal",
-//     safesearch:true
-//   });
-
-
-// return fetch(`https://pixabay.com/api/?${searchParams}`)}
-
-
-// axios.defaults.baseURL = 'https://pixabay.com/api/';
-// axios.defaults.headers.common["key"] ='42747257-6cb1908b03b224c2fc7af7612';
-
-
-return axios.get("https://pixabay.com/api/", {
+export default async function fetchPhotos(search,page){
+const response = await axios.get("https://pixabay.com/api/", {
 	params: {
     key :"42747257-6cb1908b03b224c2fc7af7612",
-    q: `"${search}"`,
+    q: search,
     image_type: "photo",
     orientation:"horizontal",
-    safesearch:true
+    safesearch:true,
+    page: page,
+    per_page: 15
 	}
     })
-  
+    return  response.data
   }
--- src/main.js
+++ src/main.js
@@ -7,34 +7,67 @@ import simpleLightbox from "simplelightbox";
 
 const form = document.querySelector('form')
 const loader = document.querySelector('.loader-css')
+const loadMoreBtn =document.querySelector('.load-button')
 const galleryLook = new simpleLightbox('.gallery a')
 
-
+loadMoreBtn.addEventListener("click", clickHandler)
 form.addEventListener("submit", submitHandler)
+
+let page;
+let searchPrase;
+let maxPage;
+
 
 function submitHandler(event){
     event.preventDefault()
-    let searchPrase = event.currentTarget.elements.search.value;
-    if (searchPrase.trim() === '')
+    let inputPrase = event.currentTarget.elements.search.value;
+    if (inputPrase.trim() === '')
     {
         const note = "Empty input"
         invokeNotification(note)
         }
     else {
-       fetchPhotos(searchPrase)
-        .then(({data: {total, hits}}) => { 
-            totalCheck(total,hits)
-        })
-        .catch((error) => console.log(error));
-        loader.classList.add("loader")
-        }  
+        searchPrase = inputPrase;
+        page = 1;
+        const photos = fetchPhotos(searchPrase, page);
+        totalCheck(photos);
+    //    fetchPhotos(searchPrase)
+    //     .then(({data: {total, hits}}) => { 
+    //         totalCheck(total,hits)
+    //     })
+    //     .catch((error) => console.log(error));
+    //     loader.classList.add("loader")
+    //     }  
         
     form.reset()
-}
+}}
 
 
-function totalCheck(total,hits){
+ 
+  
+
+ function clickHandler(){
+    if (page >= maxPage) {
+        let note = "We're sorry, there are no more posts to load"
+        return invokeNotification(note)
+    }
+        console.log("Page before fetch in click", page)
+        console.log("Max pages before fetch in click: ", maxPage)
+        page+=1;
+        const photos = fetchPhotos(searchPrase, page)
+        totalCheck(photos)
+        console.log("Page after fetch in click", page)
+        console.log("Max pages after fetch in click: ", maxPage)
+
+      }
+    
+ 
+
+function totalCheck(photosObject){
+    photosObject.then((response) => {
+        const {total, hits} = response
     if (total>0){
+        maxPage = Math.ceil(total/15)
         let imageGallary = createMarkup(hits)
         addMarkup(imageGallary);
         galleryLook.refresh()
@@ -45,15 +78,42 @@ function totalCheck(total,hits){
        addMarkup(startMarkup);
        invokeNotification(note)
        }
+})
+.catch((error) => console.log(error));
 }
+// async function fetchAndRender(searchPrase, page){
+//     console.log("page before fetchPhotos")
+//     // try{
+//    await fetchPhotos(searchPrase, page=1)
+//         .then(({data: {total, hits}}) => { 
+//                 return total, hits
+//                if ({data:{total}}>0){
+//         maxPage = Math.ceil({data: {total}}/15)
+//         page+=1;
+//         let imageGallary = createMarkup({data: {hits}})
+//         addMarkup(imageGallary);
+//         galleryLook.refresh()
+//        } 
+//            else{
+//        const note = "Sorry, there are no images matching your search query. Please try again!"
+//        const startMarkup = `<li><span class="loader-css"></span></li>`;
+//        addMarkup(startMarkup);
+//        invokeNotification(note)
+//        }
+//         }
+//         )}
+        // .catch((error) => console.log(error));
+        // loader.classList.add("loader")
+        // }  
+        // {data: {total, hits}}
 
-function responseCheck(response){
-    if (!response.ok) {
-        throw new Error(response.status);
-        }
-        let photosObject;
-            return photosObject = response.json()
-    }
+// function responseCheck(response){
+//     if (!response.ok) {
+//         throw new Error(response.status);
+//         }
+//         let photosObject;
+//             return photosObject = response.json()
+//     }
 
 function invokeNotification(message){
     (iziToast.error({
