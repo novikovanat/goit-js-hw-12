ead0765e66f1358afc89013d9aaacf9a0a5875de
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-03-10 21:02:08 +0100

    loader added

--- src/main.js
+++ src/main.js
@@ -30,35 +30,21 @@ function submitHandler(event){
         searchPrase = inputPrase;
         page = 1;
         const photos = fetchPhotos(searchPrase, page);
+        loader.classList.add("loader")
         totalCheck(photos);
-    //    fetchPhotos(searchPrase)
-    //     .then(({data: {total, hits}}) => { 
-    //         totalCheck(total,hits)
-    //     })
-    //     .catch((error) => console.log(error));
-    //     loader.classList.add("loader")
-    //     }  
-        
     form.reset()
 }}
 
-
- 
-  
 
  function clickHandler(){
     if (page >= maxPage) {
         let note = "We're sorry, there are no more posts to load"
         return invokeNotification(note)
     }
-        console.log("Page before fetch in click", page)
-        console.log("Max pages before fetch in click: ", maxPage)
         page+=1;
         const photos = fetchPhotos(searchPrase, page)
+        loader.classList.add("loader")
         totalCheck(photos)
-        console.log("Page after fetch in click", page)
-        console.log("Max pages after fetch in click: ", maxPage)
-
       }
     
  
@@ -81,39 +67,6 @@ function totalCheck(photosObject){
 })
 .catch((error) => console.log(error));
 }
-// async function fetchAndRender(searchPrase, page){
-//     console.log("page before fetchPhotos")
-//     // try{
-//    await fetchPhotos(searchPrase, page=1)
-//         .then(({data: {total, hits}}) => { 
-//                 return total, hits
-//                if ({data:{total}}>0){
-//         maxPage = Math.ceil({data: {total}}/15)
-//         page+=1;
-//         let imageGallary = createMarkup({data: {hits}})
-//         addMarkup(imageGallary);
-//         galleryLook.refresh()
-//        } 
-//            else{
-//        const note = "Sorry, there are no images matching your search query. Please try again!"
-//        const startMarkup = `<li><span class="loader-css"></span></li>`;
-//        addMarkup(startMarkup);
-//        invokeNotification(note)
-//        }
-//         }
-//         )}
-        // .catch((error) => console.log(error));
-        // loader.classList.add("loader")
-        // }  
-        // {data: {total, hits}}
-
-// function responseCheck(response){
-//     if (!response.ok) {
-//         throw new Error(response.status);
-//         }
-//         let photosObject;
-//             return photosObject = response.json()
-//     }
 
 function invokeNotification(message){
     (iziToast.error({
