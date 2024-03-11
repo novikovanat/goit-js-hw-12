f5dc417f3aa649ac719e8f9f38195f5ed3a18258
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-03-11 17:37:21 +0100

    time out removed

--- src/main.js
+++ src/main.js
@@ -1,110 +1,102 @@
-import {createMarkup, resetMarkup, addMarkupNew} from "./js/render-functions";
-import fetchPhotos from "./js/pixabay-api";
-import errorSvg from './img/error.svg'
+import { createMarkup, resetMarkup, addMarkupNew } from './js/render-functions';
+import fetchPhotos from './js/pixabay-api';
+import errorSvg from './img/error.svg';
 import iziToast from 'izitoast';
-import simpleLightbox from "simplelightbox";
+import simpleLightbox from 'simplelightbox';
 
+const form = document.querySelector('form');
+const loader = document.querySelector('.container');
+const loadMoreBtn = document.querySelector('.load-button');
+const galleryLook = new simpleLightbox('.gallery a');
 
-const form = document.querySelector('form')
-const loader = document.querySelector('.container')
-const loadMoreBtn =document.querySelector('.load-button')
-const galleryLook = new simpleLightbox('.gallery a')
-
-loadMoreBtn.addEventListener("click", clickHandler)
-form.addEventListener("submit", submitHandler)
+loadMoreBtn.addEventListener('click', clickHandler);
+form.addEventListener('submit', submitHandler);
 
 let page;
 let searchPrase;
 
 function scrollToPage() {
-    const galleryItem = document.querySelector('.gallery-item')
-    let galleryItemSize = galleryItem.getBoundingClientRect();
-    window.scrollBy({
-        top: 2* galleryItemSize.height,
-        behavior: "smooth",
-      });
+  const galleryItem = document.querySelector('.gallery-item');
+  let galleryItemSize = galleryItem.getBoundingClientRect();
+  window.scrollBy({
+    top: 2 * galleryItemSize.height,
+    behavior: 'smooth',
+  });
 }
 
-function submitHandler(event){
-    event.preventDefault()
-    loadMoreBtn.classList.add('is-hidden')
-    resetMarkup(" ")
-    let inputPrase = event.currentTarget.elements.search.value;
-    if (inputPrase.trim() === '')
-    {
-        const note = "Empty input"
-        invokeNotification(note)
-        }
-    else {
-        searchPrase = inputPrase;
-        page = 1;
-        const photos = fetchPhotos(searchPrase, page);
-        loader.classList.remove("is-hidden")
-        totalCheck(photos)
-    form.reset()
-}}
+function submitHandler(event) {
+  event.preventDefault();
+  loadMoreBtn.classList.add('is-hidden');
+  resetMarkup(' ');
+  let inputPrase = event.currentTarget.elements.search.value;
+  if (inputPrase.trim() === '') {
+    const note = 'Empty input';
+    invokeNotification(note);
+  } else {
+    searchPrase = inputPrase;
+    page = 1;
+    const photos = fetchPhotos(searchPrase, page);
+    loader.classList.remove('is-hidden');
+    totalCheck(photos);
+    form.reset();
+  }
+}
 
+function clickHandler() {
+  loadMoreBtn.classList.add('is-hidden');
+  page += 1;
+  const photos = fetchPhotos(searchPrase, page);
+  loader.classList.remove('is-hidden');
+  totalCheck(photos);
+}
 
- function clickHandler(){
-    loadMoreBtn.classList.add('is-hidden')
-        page+=1;
-        const photos = fetchPhotos(searchPrase, page)
-        loader.classList.remove("is-hidden")
-        totalCheck(photos)
-      }
-    
- 
-
-function totalCheck(photosObject){
-    photosObject.then((response) => {
-        const {total, hits} = response
-    if (total>0){
-        let imageGallary = createMarkup(hits)
-        let maxPage = Math.ceil(total/15)
-          if (maxPage <= page){
-            loadMoreBtn.classList.add('is-hidden')
-            let note = "We're sorry, but you've reached the end of search results."
-            invokeNotification(note)
-        }
-          else{ 
-            scrollToPage
-            setTimeout(() => { loadMoreBtn.classList.remove('is-hidden')
-	
-}, 3000);
-            
+function totalCheck(photosObject) {
+  photosObject
+    .then(response => {
+      const { total, hits } = response;
+      if (total > 0) {
+        let imageGallary = createMarkup(hits);
+        let maxPage = Math.ceil(total / 15);
+        if (maxPage <= page) {
+          loadMoreBtn.classList.add('is-hidden');
+          let note =
+            "We're sorry, but you've reached the end of search results.";
+          invokeNotification(note);
+        } else {
+          scrollToPage;
+            loadMoreBtn.classList.remove('is-hidden');
+          
         }
         addMarkupNew(imageGallary);
-        scrollToPage()
-        galleryLook.refresh()
-       } 
-           else{
-       const note = "Sorry, there are no images matching your search query. Please try again!"
-       const startMarkup = `<li><span class="loader-css"></span></li>`;
-       resetMarkup(startMarkup);
-       invokeNotification(note)
-       }
-    loader.classList.add("is-hidden")
-})
-.catch((error) => console.log(error));
+        scrollToPage();
+        galleryLook.refresh();
+      } else {
+        const note =
+          'Sorry, there are no images matching your search query. Please try again!';
+        const startMarkup = `<li><span class="loader-css"></span></li>`;
+        resetMarkup(startMarkup);
+        invokeNotification(note);
+      }
+      loader.classList.add('is-hidden');
+    })
+    .catch(error => console.log(error));
 }
 
-function invokeNotification(message){
-    (iziToast.error({
-        timeout:'5000',
-        messageColor:'#ffffff',
-        titleColor:"#fff",
-        titleSize:"16",
-        titleLineHeight:'24',
-        message: message,
-        iconUrl: errorSvg,
-        iconColor:'#fff',
-        backgroundColor:'#EF4040',
-        progressBarColor:"#B51B1B",
-        position:'topRight',
-        messageSize:'16',
-        messageLineHeight:'24',
-        maxWidth:'432px'
-    }));
+function invokeNotification(message) {
+  iziToast.error({
+    timeout: '5000',
+    messageColor: '#ffffff',
+    titleColor: '#fff',
+    titleSize: '16',
+    titleLineHeight: '24',
+    message: message,
+    iconUrl: errorSvg,
+    iconColor: '#fff',
+    backgroundColor: '#EF4040',
+    progressBarColor: '#B51B1B',
+    position: 'topRight',
+    messageSize: '16',
+    messageLineHeight: '24',
+    maxWidth: '432px',
+  });
 }
-
- 
